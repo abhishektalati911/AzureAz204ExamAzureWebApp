ce2a277e915505067b2a6ffd2e64b051af898a25
Abhishek Talati <abhishek.talati91@outlook.com>
2023-01-09 19:13:10 +0530

    .

--- ContinuousDeployment/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ContinuousDeployment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
